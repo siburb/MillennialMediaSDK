f99afc12c2477c6fd658b5613465e1b84eb2e65d
Simon Burbidge <simon.burbidge@mokosocialmedia.com>
2014-11-20 15:19:34 +0800

    Initial Commit

---
+++ Sample-App/MMSampleApp/Banner/BannerExampleViewController.h
@@ -0,0 +1,21 @@
+//
+//  BannerExampleViewController.h
+//  MMSampleApp
+//
+//
+//  Copyright (c) 2010-2014 Millennial Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MillennialMedia/MMAdView.h>
+
+@interface BannerExampleViewController : UIViewController {
+    MMAdView *_bannerAdView;
+    NSTimer *_timer;
+}
+
+@property (nonatomic, retain) IBOutlet UILabel *statusLabel;
+
+- (IBAction)refresh:(id)sender;
+
+@end
